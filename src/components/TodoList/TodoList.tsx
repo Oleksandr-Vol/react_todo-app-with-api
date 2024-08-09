36e41667f3e10d08228fd2ce93ed591ea8fe363b
Oleksandr <alex.volkovskyi@gmail.com>
2024-08-09 15:16:32 +0300

    add task solution

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import { Todo } from '../../types/Todo';
+import classNames from 'classnames';
+import { USER_ID } from '../../api/todos';
+
+type Props = {
+  todos: Todo[];
+  tempTodo: Todo | null;
+  onDelete: (todoId: number) => void;
+  idOfDeletedTodo: number[];
+  updateTodo: (todo: Todo) => Promise<void>;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  tempTodo,
+  onDelete,
+  idOfDeletedTodo,
+  updateTodo,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(({ id, title, completed }) => {
+        const handleStatusChange = (
+          event: React.ChangeEvent<HTMLInputElement>,
+        ) => {
+          updateTodo({
+            id,
+            userId: USER_ID,
+            title,
+            completed: event.target.checked,
+          });
+        };
+
+        return (
+          <div
+            key={id}
+            data-cy="Todo"
+            className={classNames('todo', { completed: completed })}
+          >
+            <label className="todo__status-label">
+              <input
+                data-cy="TodoStatus"
+                type="checkbox"
+                className="todo__status"
+                // checked={completed}
+                onChange={handleStatusChange}
+              />
+            </label>
+
+            <span data-cy="TodoTitle" className="todo__title">
+              {title}
+            </span>
+
+            <button
+              type="button"
+              className="todo__remove"
+              data-cy="TodoDelete"
+              onClick={() => onDelete(id)}
+            >
+              ×
+            </button>
+
+            <div
+              data-cy="TodoLoader"
+              className={classNames('modal', 'overlay', {
+                'is-active': idOfDeletedTodo.includes(id),
+              })}
+            >
+              <div className="modal-background has-background-white-ter" />
+              <div className="loader" />
+            </div>
+          </div>
+        );
+      })}
+
+      {tempTodo && (
+        <div data-cy="Todo" className="todo">
+          <label className="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              className="todo__status"
+            />
+          </label>
+
+          <span data-cy="TodoTitle" className="todo__title">
+            {tempTodo?.title}
+          </span>
+          <button type="button" className="todo__remove" data-cy="TodoDelete">
+            ×
+          </button>
+
+          <div data-cy="TodoLoader" className="modal overlay is-active">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
